9986a8d43c4b80c8e6b7b7670fb7b6d9e8970c05
SANJAY NAVEEN <sanjaynaveen@SANJAYs-MacBook-Air.local>
2026-06-09 15:12:07 +0530

    Fix Netlify deployment: remove webpack devDependency, set CI=false, and configure Node/npm

--- src/config.js
+++ src/config.js
@@ -20,9 +20,9 @@ import axios from "axios";
 // export const server = "http://192.168.29.44:3004"
 
 
-export const IMAGE_BASE_URL = "http://localhost:4002/api/public";
-export const BASE_URL = "http://localhost:4002/api/mobile";
-export const server = "http://localhost:4002"
+export const IMAGE_BASE_URL = "https://api.gripforum.com/api/public/";
+export const BASE_URL = "https://api.gripforum.com/api/mobile";
+export const server = "https://api.gripforum.com/"
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
